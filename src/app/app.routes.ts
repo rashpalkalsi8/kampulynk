778ad57a3cb844061ec1819c93e05dba6d9cae47
kampulynk <rashpalkalsi8@gmail.com>
2025-09-12 20:54:17 +0530

    New

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Homepage } from './pages/homepage/homepage';
 import { AboutUs } from './pages/about-us/about-us';
 import { ContactUs } from './pages/contact-us/contact-us';
+import { OurTeam } from './pages/our-team/our-team';
 
 export const routes: Routes = [
     { path: '', component: Homepage },         
   { path: 'about', component: AboutUs },
   { path: 'contact', component: ContactUs },
+  { path: 'team', component: OurTeam },
 ];
---
+++ src/app/pages/our-team/our-team.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-team',
+  imports: [],
+  templateUrl: './our-team.html',
+  styleUrl: './our-team.scss'
+})
+export class OurTeam {
+
+}
